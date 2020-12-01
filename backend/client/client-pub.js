f67fd469c4c73e03824d68d47063267f5eab36bc
Rolando Franco <frannrolando96@gmail.com>
2020-11-30 22:54:30 -0300

    Mergeo parse.

--- backend/client/client-pub.js
+++ backend/client/client-pub.js
@@ -1,16 +1,15 @@
 var zmq = require('zeromq')
   , sock = zmq.socket('pub');
-  const IP= '127.0.0.1';
-  const PORT=  '3000';
+const IP = '127.0.0.1';
+const PORT = '3000';
 
-const zmq = require('zeromq'),
-			sock = zmq.socket('pub')
+sock = zmq.socket('pub')
 
-sock.connect('tcp://'+IP+':'+PORT+'')
+sock.connect('tcp://' + IP + ':' + PORT + '')
 
 let counter = 0
 
-setInterval(function() {
-	console.log('Envia mensaje nº', counter)
+setInterval(function () {
+  console.log('Envia mensaje nº', counter)
   sock.send(['miTopico2', 'Este es el mensaje ' + counter++])
 }, 500)
